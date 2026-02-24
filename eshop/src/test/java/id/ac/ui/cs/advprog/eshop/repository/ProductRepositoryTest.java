4c61f6cf7a97fa1c509c9a02ca4ce8aee7c06473
applepiesss <nadiaaiiisyahf@gmail.com>
2026-02-24 14:31:42 +0700

    [2] fixed issue

--- eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -136,7 +136,7 @@ class ProductRepositoryTest {
 
         Product editedProduct = productRepository.findProductById("eb558e9f-1c39-460e-8860-71af6af63bd6");
 
-        assertEquals(editedProduct.getProductId(), "eb558e9f-1c39-460e-8860-71af6af63bd6");
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", editedProduct.getProductId());
         assertEquals("Sampo Cap Usep", editedProduct.getProductName());
         assertEquals(50, editedProduct.getProductQuantity());
     }
