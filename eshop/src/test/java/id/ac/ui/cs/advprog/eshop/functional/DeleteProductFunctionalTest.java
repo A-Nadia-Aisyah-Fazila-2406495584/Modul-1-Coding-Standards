ff0a9fac9010a2584ad2e22b4369469dd0534fd2
applepiesss <nadiaaiiisyahf@gmail.com>
2026-02-24 17:44:34 +0700

    [12] Fixed issue

--- eshop/src/test/java/id/ac/ui/cs/advprog/eshop/functional/DeleteProductFunctionalTest.java
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/functional/DeleteProductFunctionalTest.java
@@ -31,7 +31,7 @@ public class DeleteProductFunctionalTest {
     }
 
     @Test
-    void deleteProduct_isSuccessful(ChromeDriver driver) throws Exception {
+    void deleteProduct_isSuccessful(ChromeDriver driver) {
         driver.get(baseUrl + "/product/create");
         driver.findElement(By.id("nameInput")).sendKeys("Sampo Cap");
         driver.findElement(By.id("quantityInput")).sendKeys("10");
