c1ee0807e96d2de3d1b027c6db4d151c87bd2316
applepiesss <nadiaaiiisyahf@gmail.com>
2026-02-24 08:03:51 +0700

    Test trigger sonar

--- eshop/build.gradle.kts
+++ eshop/build.gradle.kts
@@ -6,6 +6,7 @@ val junitJupiterVersion = "5.9.1"
 plugins {
 	java
 	jacoco
+	id("org.sonarqube") version "5.1.0.4882"
 	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
 }
