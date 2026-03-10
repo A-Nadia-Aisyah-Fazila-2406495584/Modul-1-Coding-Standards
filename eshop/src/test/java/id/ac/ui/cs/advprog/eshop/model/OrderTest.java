9ad977a3043b98824940e9a16b5bde41d0ad9ecb
applepiesss <nadiaaiiisyahf@gmail.com>
2026-03-10 12:54:33 +0700

    [GREEN] Implement Order model

--- eshop/src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -38,7 +38,7 @@ class OrderTest {
 
     @Test
     void testCreateOrderDefaultStatus() {
-        Order order = new Order("13652556-012a-4c30-b546-54eb1396d79b",
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat");
 
         assertSame(this.products, order.getProducts());
